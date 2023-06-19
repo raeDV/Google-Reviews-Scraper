355d4bcb919a3e432714d266dad84a2cfdf4b38e
humphrey777 <song.heping01@gmail.com>
2023-06-19 11:35:27 -0400

    adding 2 demos, one is a CLI that generate csv output file and one is for front-end that returns jason. Please replace 'Your_API_Key' to your real google API key for testing

---
+++ test_jason_frontend.py
@@ -0,0 +1,90 @@
+import requests
+from flask import Flask, render_template, request
+import datetime
+
+app = Flask(__name__)
+
+
+def calculate_time_description(time_seconds):
+    current_time = datetime.datetime.now()
+    review_time = datetime.datetime.fromtimestamp(time_seconds)
+    time_diff = current_time - review_time
+    days = time_diff.days
+    if days > 0:
+        return f"{days} days ago"
+    else:
+        seconds = time_diff.seconds
+        hours = seconds // 3600
+        minutes = (seconds // 60) % 60
+        if hours > 0:
+            return f"{hours} hours ago"
+        elif minutes > 0:
+            return f"{minutes} minutes ago"
+        else:
+            return "Just now"
+
+
+def get_google_reviews(place):
+    url = 'https://maps.googleapis.com/maps/api/place/findplacefromtext/json'
+    params = {
+        'input': place,
+        'inputtype': 'textquery',
+        'fields': 'name,formatted_address,place_id',
+        # Replace 'Your_API_Key' with your real google API key for testing
+        'key': 'Your_API_Key'
+    }
+    response = requests.get(url, params=params)
+    data = response.json()
+
+    if 'candidates' in data and len(data['candidates']) > 0:
+        place_id = data['candidates'][0]['place_id']
+        return fetch_reviews(place_id)
+    else:
+        return []
+
+
+def fetch_reviews(place_id):
+    url = 'https://maps.googleapis.com/maps/api/place/details/json'
+    params = {
+        'place_id': place_id,
+        'fields': 'name,rating,reviews',
+        # Replace 'Your_API_Key' with your real google API key for testing
+        'key': 'Your_API_Key'
+    }
+    response = requests.get(url, params=params)
+    data = response.json()
+
+    if 'result' in data and 'reviews' in data['result']:
+        reviews = data['result']['reviews']
+        extracted_reviews = []
+        for review in reviews:
+            rating = review.get('rating', None)
+            comment = review.get('text', None)
+            if rating and comment:
+                author_name = review.get('author_name', None)
+                time_seconds = review.get('time', None)
+                time_description = calculate_time_description(time_seconds)
+                extracted_reviews.append({
+                    'rating': rating,
+                    'comment': comment,
+                    'author_name': author_name,
+                    'time_description': time_description
+                })
+        return extracted_reviews
+    else:
+        return []
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        place = request.form.get('place')
+        if place:
+            reviews = get_google_reviews(place)
+            return render_template('index.html', reviews=reviews, place=place)
+
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
