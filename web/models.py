551602acf665fa17572faa33921af6dadca496ea
chester <chesterraedevera@gmail.com>
2023-06-21 21:22:36 -0400

    updated

---
+++ web/models.py
@@ -0,0 +1,13 @@
+from web import db # from . means from this package or from web (web is the package name)import db
+from flask_login import UserMixin
+
+class User(db.Model, UserMixin):
+    id=db.Column(db.Integer, primary_key=True)
+    email= db.Column(db.String(100), unique=True)
+    userName = db.Column(db.String(100))
+    password=db.Column(db.String(100))
+    # searches=db.relationship('Search')
+
+
+
+
